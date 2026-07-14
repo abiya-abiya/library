90fc4f6b685544435fbfea6f987ceb27d183689e
Abdulloh <anuraliyev71@gmail.com>
2026-07-14 09:19:26 +0500

    ikkinchi normotiv

--- books/models.py
+++ books/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    year = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
