570b39349977024331e822c9622f219dc45ff925
Abdulloh <anuraliyev71@gmail.com>
2026-07-14 09:29:14 +0500

    uchunchi normotiv

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from books import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('book_list', views.book_list, name='book_list'),
 ]
 
 
--- books/views.py
+++ books/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from books.models import Book
+
+
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def book_list(request):
+    books = Book.objects.all()
+    context = {
+        'books': books
+    }
+    return render(request, "book_list.html", context)
